d303e86eda95d1c02446e111aac42c32b407b71b
Aanya <Aanya@Anishs-MacBook-Air.local>
2021-10-10 10:49:07 +0530

    third commit

---
+++ TISB_HACKS /basic_app/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+    url(r'^about',views.AboutView.as_view(), name='about_us'),
+    url(r'^list',views.DoctorListView.as_view(),name='doctor_list'),
+    url(r'^doctor/(?P<pk>\d+)', views.DoctorDetailView.as_view(), name='doctor_detail'),
+    url(r'^doctorcomment/(?P<pk>\d+)/$', views.add_comment_to_doctor, name='add_comment_to_doctor'),
+    url(r'^doctorreply/(?P<pk>\d+)/$', views.add_reply_to_comment, name='add_reply_to_comment'),
+    url(r'^doctor/new/', views.CreateDoctorView.as_view(), name='doctor_new'),
+    url(r'^doctoredit/(?P<pk>\d+)/$', views.DoctorUpdateView.as_view(), name='doctor_edit'),
+    url(r'^doctorremove/(?P<pk>\d+)/$', views.DoctorDeleteView.as_view(), name='doctor_remove'),
+    url(r'^doctorpatient/(?P<pk>\d+)/$', views.add_patient_to_dotctor, name='add_patient_to_dotctor'),
+    url(r'^patient_detail_view/(?P<pk>\d+)/$',views.PatientDetailView.as_view(), name='patient_detail_view'),
+    url(r'^$',views.HomePage.as_view(), name='home_page'),
+]
---
+++ TISB_HACKS /basic_app/views.py
@@ -0,0 +1,94 @@
+from django.shortcuts import render
+from django.views.generic import (TemplateView,ListView,
+                                  DetailView,CreateView,
+                                  UpdateView,DeleteView)
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from basic_app.forms import DoctorForm, PatientForm, CommentForm, ReplyForm
+from basic_app.models import Doctor,Patient, Comment, Reply
+
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+
+
+class HomePage(TemplateView):
+    template_name = 'home.html'
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+
+class DoctorListView(ListView):
+    model=Doctor
+
+class DoctorDetailView(DetailView):
+    model=Doctor
+
+# class PatientDetailView(DetailView):
+#     model=Patient
+
+
+class CreateDoctorView(LoginRequiredMixin,CreateView):
+    login_url = '/login/'
+
+    redirect_field_name='basic_app/doctor_detail.html'
+    form_class=DoctorForm
+    model=Doctor
+
+class DoctorUpdateView(LoginRequiredMixin,UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'basic_app/doctor_detail.html'
+
+    form_class = DoctorForm
+    model = Doctor
+
+class DoctorDeleteView(LoginRequiredMixin,DeleteView):
+    model = Doctor
+    success_url = reverse_lazy('doctor_list')
+
+class PatientDetailView(LoginRequiredMixin, DetailView):
+    model= Patient
+
+
+## Functions
+
+def add_patient_to_dotctor(request, pk):
+    doctor = get_object_or_404(Doctor, pk=pk)
+    if request.method == "POST":
+        form = PatientForm(request.POST)
+        if form.is_valid():
+            patient = form.save(commit=False)
+            patient.doctor = doctor
+            patient.save()
+            return redirect('doctor_detail', pk=doctor.pk)
+    else:
+        form = PatientForm()
+    return render(request, 'basic_app/patient_form.html', {'form': form})
+
+def add_comment_to_doctor(request, pk):
+    doctor = get_object_or_404(Doctor, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.doctor = doctor
+            comment.save()
+            return redirect('doctor_detail', pk=doctor.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'basic_app/comment_form.html', {'form': form})
+
+def add_reply_to_comment(request, pk):
+    # //doctor = get_object_or_404(Doctor, pk=pk)
+    comment = get_object_or_404(Comment, pk=pk)
+    if request.method == "POST":
+        form = ReplyForm(request.POST)
+        if form.is_valid():
+            reply = form.save(commit=False)
+            reply.comment = comment
+            reply.save()
+            return redirect('doctor_detail', pk=comment.doctor.pk)
+    else:
+        form = ReplyForm()
+    return render(request, 'basic_app/reply_form.html', {'form': form})
