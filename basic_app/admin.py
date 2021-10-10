6dccc4eaa223aed00241bf13d005b3fb703c35c4
meaninglessC0de <84666166+meaninglessC0de@users.noreply.github.com>
2021-10-10 10:43:53 +0530

    Delete basic_app directory

--- basic_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from basic_app.models import Doctor, Patient
-# Register your models here.
-
-admin.site.register(Doctor)
-admin.site.register(Patient)
